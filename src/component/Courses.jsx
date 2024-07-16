931b768164e5b0c42569a30c16125a741a46629e
Rajalakshmy S <rajalakshmys27@gmail.com>
2024-07-16 21:39:31 +0530

    add fix for default page content selection

--- src/component/Courses.jsx
+++ src/component/Courses.jsx
@@ -1,5 +1,10 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useContext, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Sidebar from "./viewcourse/Sidebar";
 import DocumentationPage from "./viewcourse/DocumentationPage";
 import htmlPages from "./doc/html";
@@ -9,22 +14,35 @@ import Header from "./viewcourse/Header";
 import AppContext from "./viewcourse/AppContext";
 
 const Courses = () => {
+  const [defaultPageItems, setDefaultPageItems] = useState({
+    defaultPageTitle: htmlPages[0].title,
+    defaultPageContent: htmlPages[0].content,
+    defaultPageDetails: htmlPages[0],
+  });
   const { selectedDocType } = useContext(AppContext);
 
   const getPages = () => {
     switch (selectedDocType) {
-      case "html":
-        return htmlPages;
       case "css":
         return cssPages;
       case "react":
         return reactPages;
+      case "html":
       default:
         return htmlPages;
     }
   };
 
   const pages = getPages();
+
+  useEffect(() => {
+    const defaultPage = pages[0];
+    setDefaultPageItems({
+      defaultPageTitle: defaultPage.title,
+      defaultPageContent: defaultPage.content,
+      defaultPageDetails: defaultPage,
+    });
+  }, [selectedDocType, pages]);
 
   return (
     <div>
@@ -37,6 +55,16 @@ const Courses = () => {
 
           <div className="main-content">
             <Routes>
+              <Route
+                path="/"
+                element={
+                  <DocumentationPage
+                    pageTitle={defaultPageItems.defaultPageTitle}
+                    pageContent={defaultPageItems.defaultPageContent}
+                    pageDetails={defaultPageItems.defaultPageDetails}
+                  />
+                }
+              />
               {pages.map((page) => (
                 <Route
                   key={page.id}
